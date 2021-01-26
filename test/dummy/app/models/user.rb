aa3e9ec4d3d944c11b09256885284379cd937662
Jason Lee <huacnlee@gmail.com>
2021-01-26 19:39:54 +0800

    Improve test and upgrade dummary app into Rails 6.1

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
-  has_many_attached :files
+  has_one_attached :avatar, service: :aliyun
+  has_many_attached :files, service: :aliyun
 end
